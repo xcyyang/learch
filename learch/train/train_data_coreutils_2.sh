8c6f8b1aca1c33a6a70d0dffa0032bda907ca2a6
yxcy <48614259+xcyyang@users.noreply.github.com>
2022-04-05 01:34:36 -0400

    Update train_data_coreutils_2.sh

--- learch/train/train_data_coreutils_2.sh
+++ learch/train/train_data_coreutils_2.sh
@@ -5,7 +5,7 @@ PROG_LIST=${1}
 STRATEGIES=('sgs:1' 'sgs:2' 'sgs:4')
 for strategy in "${STRATEGIES[@]}"
 do
-       cat ${PROG_LIST} | while read prog
+       tac ${PROG_LIST} | while read prog
        do
                ./train_data.sh ${prog} ~/train_data 1800 0 ${strategy}
        done
